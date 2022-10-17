21505a66b0a6666b90103d42a0476008e883acbe
GIDRO32 <pambossable@gmail.com>
2022-10-17 19:12:49 +0300

    Add code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,77 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Players playersTag = new Players();
+        ArrayList<Players> players = new ArrayList<>();
+        Scanner key = new Scanner(System.in);
+        boolean isTyping = true;
+        String symbol = null;
+        String option = null;
+        int find = 1;
+        System.out.println("Welcome to the Football Players collection app.");
+        while (isTyping) {
+            System.out.println("\nType a word to choose an option.");
+            System.out.println("\nexit - exit program\nadd player - add player to the collection\nprint - print all players\nsort - sort players by goals\nfind team- find players by team name\nfind nation - find players by nation\nfind name - find players by their names");
+            option = key.nextLine();
+
+            if (option.toLowerCase().equals("exit")) {
+                break;
+            } else if (option.toLowerCase().equals("add player")) {
+                System.out.println("Type a player data(name, surname, team-name, Player's nation, number of goals.)\nNote: if Team name or country has more than one word, divide them with a hyphen.\nFor example: Let-the-bodies-hit-the-floor");
+                String playerIn = key.nextLine();
+                Players pl = new Players();
+                pl.stringToProperties(playerIn);
+                players.add(pl);
+
+            }
+            System.out.println("Continue? Y/N");
+            String answer = key.nextLine();
+
+            if (answer.toLowerCase().equals("y")) {
+                isTyping = true;
+                option = null;
+                System.out.println("\nChoose an option.\nexit - exit program\nadd player - add player to the collection\nprint - print all players\nsort - sort players by goals\nfind team- find players by team name\nfind nation - find players by nation\nfind name - find players by their names");
+                option = key.nextLine();
+            } else
+            {
+                isTyping = false;
+            }
+            int i = 0;
+            if (option.toLowerCase().equals("print"))
+            {
+                for (i = 0; i < players.size(); i++)
+                {
+                    System.out.println("\nPlayer " + (i + 1) + ":");
+                    players.get(i).printFullInfo();
+                }
+            }
+            else if (option.toLowerCase().equals("sort"))
+            {
+                for (i = 0; i < players.size(); i++) {
+                    System.out.println("\nPlayer " + (i + 1) + ":");
+                    playersTag.sortPlayers();
+                    players.get(i).printFullInfo();
+                }
+            }
+            else if (option.toLowerCase().equals("find team"))
+            {
+                symbol = key.nextLine();
+                playersTag.findPlayerTeam(symbol);
+            }
+            else if (option.toLowerCase().equals("find nation"))
+            {
+                symbol = key.nextLine();
+                playersTag.findPlayerNation(symbol);
+            }
+            else if (option.toLowerCase().equals("find name"))
+            {
+                symbol = key.nextLine();
+                System.out.println(symbol);
+                playersTag.findPlayerName(symbol);
+            }
+        }
     }
+
 }
--- src/Players.java
+++ src/Players.java
@@ -1,2 +1,80 @@
-package PACKAGE_NAME;public class Players {
+public class Players
+{
+    private Players[] player;
+    public String name;
+    public String surname;
+    public String teamName;
+    public String nation;
+    public int goals;
+    private int length = 0;
+    private int option = 0;
+    public void printFullInfo()
+    {
+        System.out.println("Full Name: " + name + " " + surname + "\nTeam Name: " + teamName + "\nNation: " + nation + "\nGoals: " + goals);
+    }
+    public void sortPlayers()
+    {
+        boolean isSorted = false;
+        int outIterations = 0;
+
+        while (!isSorted)
+        {
+            isSorted = true;
+
+            for (int j = 0; j < length - outIterations - 1; j++)
+            {
+                if (player[j].goals > player[j + 1].goals)
+                {
+                    isSorted = false;
+                    // Swap the elements
+                    Players temp = player[j];
+                    player[j] = player[j + 1];
+                    player[j + 1] = temp;
+                }
+
+            }
+            outIterations++;
+        }
+    }
+    public void findPlayerName(String searchKey)
+    {
+        for (int i = 0; i < length; i++)
+            if (player[i].name.contains(searchKey))
+                player[i].printFullInfo();
+    }
+    public void findPlayerTeam(String searchKey)
+    {
+        for (int i = 0; i < length; i++)
+            if (player[i].teamName.contains(searchKey))
+                player[i].printFullInfo();
+    }
+    public void findPlayerNation(String searchKey)
+    {
+        for (int i = 0; i < length; i++)
+            if (player[i].nation.contains(searchKey))
+                player[i].printFullInfo();
+    }
+    public void addPlayer(Players arg)
+    {
+
+        // increasing array by 1 element every time addPlayer() is called
+        // Also possible to uses Arrays.copyOf()
+        Players[] arr = new Players[length+1];
+        for(int i=0; i < arr.length-1; i++)
+        {
+            arr[i] = player[i];
+        }
+        arr[length] = arg;
+        length++;
+        player = arr;
+    }
+    public void stringToProperties(String playerTag)
+    {
+        String[] playerArr = playerTag.trim().split(" ");
+        this.name = playerArr[0];
+        this.surname = playerArr[1];
+        this.teamName = playerArr[2];
+        this.nation = playerArr[3];
+        this.goals = Integer.parseInt(playerArr[4]);
+    }
 }
